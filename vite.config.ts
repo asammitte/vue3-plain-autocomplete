b098196dd5b6298c8b3e40bb9e49798f024900fe
Kostya <pavli4enko30@gmail.com>
2023-06-20 21:00:13 +0200

    configure vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,44 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import typescript2 from "rollup-plugin-typescript2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    typescript2({
+      check: false,
+      include: ["src/components/*.vue"],
+      tsconfigOverride: {
+        compilerOptions: {
+          sourceMap: true,
+          declaration: true,
+          declarationMap: true,
+        },
+        exclude: ["vite.config.ts", "main.ts"],
+      },
+    }),
   ],
+  build: {
+    outDir: "./dist",
+    cssCodeSplit: false,
+    lib: {
+      entry: "./src/PaginationPlugin.ts",
+      formats: ["es", "cjs"],
+      name: "PaginationPlugin",
+      fileName: (format) => (format === "es" ? "index.js" : "index.cjs"),
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+        exports: "named"
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
